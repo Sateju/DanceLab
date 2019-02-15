61f353784fd83c143fbeb3b272973c0d29241d80
jjtelechea <jjtelechea@opentrends.net>
2019-02-16 00:38:01 +0100

    BaseActivity

--- app/src/main/kotlin/jjtelechea/dancelab/ui/events/EventsActivity.kt
+++ app/src/main/kotlin/jjtelechea/dancelab/ui/events/EventsActivity.kt
@@ -2,13 +2,13 @@ package jjtelechea.dancelab.ui.events
 
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import jjtelechea.dancelab.R
+import jjtelechea.dancelab.core.base.BaseActivity
 import jjtelechea.dancelab.databinding.ActivityEventsBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class EventsActivity : AppCompatActivity() {
+class EventsActivity : BaseActivity() {
 
     // Lazy Inject ViewModel
     private val eventsViewModel: EventsViewModel by viewModel()
