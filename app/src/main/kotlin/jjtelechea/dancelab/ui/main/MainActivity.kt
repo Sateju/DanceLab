b5d7b2d6ec50c8f825b6a3836c2cd6faf91ee415
jjtelechea <jjtelechea@opentrends.net>
2019-02-16 01:55:30 +0100

    add toolbar

--- app/src/main/kotlin/jjtelechea/dancelab/ui/main/MainActivity.kt
+++ app/src/main/kotlin/jjtelechea/dancelab/ui/main/MainActivity.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import jjtelechea.dancelab.R
 import jjtelechea.dancelab.core.base.BaseActivity
-import jjtelechea.dancelab.databinding.ActivityEventsBinding
+import jjtelechea.dancelab.databinding.ActivityMainBinding
 import jjtelechea.dancelab.ui.events.EventsViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
+
+    companion object {
+        private const val TITLE = "Eventos"
+    }
 
     // Lazy Inject ViewModel
     private val eventsViewModel: EventsViewModel by viewModel()
@@ -17,7 +21,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
-        val binding: ActivityEventsBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainToolbar.title = TITLE
         Log.d("MainActivity", eventsViewModel.getEventTitleViewModel())
     }
 }
