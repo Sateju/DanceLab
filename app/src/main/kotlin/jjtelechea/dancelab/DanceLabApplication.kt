463387b5737dc67b63264121b7edb634e59b4abc
jjtelechea <jjtelechea@opentrends.net>
2019-02-18 20:16:18 +0100

    implements leakCanary

--- app/src/main/kotlin/jjtelechea/dancelab/DanceLabApplication.kt
+++ app/src/main/kotlin/jjtelechea/dancelab/DanceLabApplication.kt
@@ -1,6 +1,7 @@
 package jjtelechea.dancelab
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import jjtelechea.dancelab.core.di.modules.repositoryModule
 import jjtelechea.dancelab.core.di.modules.useCaseModule
 import jjtelechea.dancelab.core.di.modules.viewModelModule
@@ -10,6 +11,11 @@ class DanceLabApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Koin
         startKoin(this, listOf(repositoryModule, useCaseModule, viewModelModule))
+
+        // Leak Canary
+        if (!LeakCanary.isInAnalyzerProcess(this)) LeakCanary.install(this)
     }
 }
