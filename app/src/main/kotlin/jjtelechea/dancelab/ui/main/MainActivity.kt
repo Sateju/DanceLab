64d3721ca335aaf5cec32f0075cbe8a18096dd53
jjtelechea <jjtelechea@opentrends.net>
2019-02-19 00:14:58 +0100

    implements on item click

--- app/src/main/kotlin/jjtelechea/dancelab/ui/main/MainActivity.kt
+++ app/src/main/kotlin/jjtelechea/dancelab/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package jjtelechea.dancelab.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import jjtelechea.dancelab.R
 import jjtelechea.dancelab.core.base.BaseActivity
 import jjtelechea.dancelab.databinding.ActivityMainBinding
+import jjtelechea.dancelab.ui.events.EventsFragment
 import jjtelechea.dancelab.ui.events.EventsViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -18,11 +20,42 @@ class MainActivity : BaseActivity() {
     // Lazy Inject ViewModel
     private val eventsViewModel: EventsViewModel by viewModel()
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.mainToolbar.title = TITLE
-        Log.d("MainActivity", eventsViewModel.getEventTitleViewModel())
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setClickListenerToBottomNavigation()
+    }
+
+    private fun setClickListenerToBottomNavigation() {
+        val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.main_bottom_navigation_events -> {
+                    openFragment(EventsFragment.newInstance(), EventsFragment.FRAGMENT_TITLE)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.main_bottom_navigation_calendar -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.main_bottom_navigation_capsules -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.main_bottom_navigation_more -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            return@OnNavigationItemSelectedListener false
+        }
+        binding.mainBottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private fun openFragment(fragment: Fragment, title: String) {
+        binding.mainToolbar.title = title
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
