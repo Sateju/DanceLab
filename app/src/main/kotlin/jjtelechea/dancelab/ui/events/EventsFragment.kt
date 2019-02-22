015c23eadbd6e3cac9cf97832ccc58f7dc9795f6
jjtelechea <jjtelechea@opentrends.net>
2019-02-22 18:57:41 +0100

    events fragment with databinding

--- app/src/main/kotlin/jjtelechea/dancelab/ui/events/EventsFragment.kt
+++ app/src/main/kotlin/jjtelechea/dancelab/ui/events/EventsFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import jjtelechea.dancelab.R
+import jjtelechea.dancelab.databinding.FragmentEventsBinding
 
 class EventsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-        inflater.inflate(R.layout.fragment_events, container, false)
 
     companion object {
         const val FRAGMENT_TITLE = "Eventos"
         fun newInstance() = EventsFragment()
+    }
+
+    private lateinit var binding: FragmentEventsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_events, container, false)
+        return binding.root
     }
 }
